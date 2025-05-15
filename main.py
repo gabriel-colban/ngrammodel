10a0a7741637a17f8cc57b2673af301b80820b05
Ron <itsronmydudes@gmail.com>
2025-05-15 10:39:24 +0200

    Added interpolation, loading all models at start, and primitive UI

--- main.py
+++ main.py
@@ -4,13 +4,24 @@ import pandas as pd
 import re
 import random
 
-# Exploration
-df = pd.read_csv("archive/True.csv")
-
-print(df.head())
-print(df["text"].head())
 
 
+# Initialising ngram models
+def load_model(n):
+    try:
+        with open(str(n)+'gram_counter.pickle', 'rb') as handle:
+            return pickle.load(handle)
+    except FileNotFoundError:
+        print(f"{n} gram model not found")
+        return None
+
+ngram_models = {}
+
+for n in range(1,6):
+    model = load_model(n)
+    if model:
+        print(f"{n} gram model loaded")
+        ngram_models[n] = model
 
 def training_data(df, test = False):
     # df contains title, date, etc. which is not needed for the ngram model
@@ -51,7 +62,7 @@ def training(df, save=False, n=2):
 
     return ngram_counter
 
-def weighted_choice(candidates):
+def weighted_choice(candidates, temperature = 1):
     # Chooses the next most common word probabilistically to ensure unique text generation
 
     total = sum(candidates.values())
@@ -64,6 +75,8 @@ def weighted_choice(candidates):
     return random.choice(list(candidates.keys()))
 
 def predict_next_word(text, counter, n=2):
+
+    # replaced by interpolated_next_word
     words = text.split()
     if len(words) < n - 1:
         print("Not enough words to predict", words)
@@ -80,38 +93,59 @@ def predict_next_word(text, counter, n=2):
 
     return weighted_choice(candidates)
 
-def predict_text(text = "", n=2, counter=Counter(), length = 30):
+def interpolated_next_word(text, n):
+    words = text.split()
+
+    candidates = {}
+
+    weights = {
+        2: 0.025,
+        3: 0.05,
+        4: 0.225,
+        5: 0.7,
+    }
+
+    for ngram in range(1, n+1):
+        context = tuple(words[-(ngram - 1):])  # last n-1 words
+        print(context)
+        model = ngram_models.get(ngram, None)
+
+        if model is None:
+            continue
+
+        weight = weights.get(ngram)
+
+        new_candidates = {ngram[-1]: count*weight for ngram, count in ngram_models[ngram].items() if ngram[:-1] == context}
+
+        candidates.update(new_candidates)
+
+    return weighted_choice(candidates)
+
+
+def predict_text(text = "", n=2, length = 30):
     words = text.split()
     if len(words) < length:
-        most_common = predict_next_word(text, counter, n=n)
+        most_common = interpolated_next_word(text, n=n)
         if most_common is None:
             print("Prediction ended early")
             return text
         text += " " + most_common
-        return predict_text(text, n=n, counter=counter, length=length)
+        return predict_text(text, n=n, length=length)
     else:
         return text
 
-def load_model(n):
-    with open(str(n)+'gram_counter.pickle', 'rb') as handle:
-        if handle:
-            return pickle.load(handle)
-        else:
-            print("No model found")
+def train_model(n=5):
+    # Exploration
+    df = pd.read_csv("archive/True.csv")
+
+    print(df.head())
+    print(df["text"].head())
+
+    df = training_data(df)
+    ngram = training(df, save=True, n=n)
 
 
-# Training
-
-n = 5
-df = training_data(df)
-ngram = training(df, save=True, n=n)
-
-# Predicting
-
-ngram = load_model(n)
-
-start = "donald trump has said"
-
-prediciton = predict_text(start, n, ngram)
-
-print(f"------------------\n\nPREDICTION COMPLETE\n\nInput: {start}\n\nOutput: {prediciton}")
+while True:
+    text = input("Enter text: ")
+    result = predict_text(text, 5, 15)
+    print("Model predicted: ", result)
